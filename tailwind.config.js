4eab7504dffb7a4e11b6bad0b57813782d885f37
Clara Coatanoan <ccoatanoan@gmail.com>
2023-04-27 21:37:36 +0200

    added header, buttons, iban

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      Overpass : ['Overpass'],
+    },
+    colors: {
+      transparent : 'transparent',
+      orange : '#DE5A10',
+      greendark : '#407A4D',
+      greenlight : '#68B278',
+      white : '#fff',
+    },
+    screens: {
+      'tablet': '640px',
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
